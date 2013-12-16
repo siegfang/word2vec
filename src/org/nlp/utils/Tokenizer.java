71aeb46c5160d7b58b1cd30f8f24cb00f731d5ec
siegfang <siegfang@gmail.com>
2013-12-16 17:28:32 +0800

    add comment for function

--- src/org/nlp/utils/Tokenizer.java
+++ src/org/nlp/utils/Tokenizer.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.ListIterator;
 
 /**
- * Created by fangy on 13-12-16.
- *
+ * Created by fangy on 13-12-13.
+ * 分割和输出记号
  */
 public class Tokenizer {
 
@@ -21,25 +21,46 @@ public class Tokenizer {
         tokenIter = tokens.listIterator();
     }
 
+    /**
+     * 分割一段文本，得到一列记号
+     * @param text 文本
+     * @param delim 分割符
+     */
     public Tokenizer(String text, String delim){
 
         tokens = Arrays.asList(text.split(delim));
         tokenIter = tokens.listIterator();
     }
 
+    /**
+     * 获得记号的数量
+     * @return 数量
+     */
     public int size(){
         return tokens.size();
     }
 
+    /**
+     * 遍历记号时，查询是否还有记号未遍历
+     * @return 若还有记号未遍历，则返回true，否则返回false。
+     */
     public boolean hasMoreTokens(){
         return tokenIter.hasNext();
     }
 
+    /**
+     * 遍历记号时获得下一个之前未遍历的记号
+     * @return 记号
+     */
     public String nextToken(){
 
         return tokenIter.next();
     }
 
+    /**
+     * 向原有记号序列的末尾添加一个记号
+     * @param token 待添加的记号
+     */
     public void add(String token){
 
         if (token == null){
@@ -49,7 +70,11 @@ public class Tokenizer {
         tokens.add(token);
     }
 
-
+    /**
+     * 以分割符连接记号并输出
+     * @param delim 分割符
+     * @return 记号由分割符连接的字符串
+     */
     public String toString(String delim){
         StringBuilder sb = new StringBuilder();
         if (tokens.size() < 1){
@@ -63,5 +88,6 @@ public class Tokenizer {
 
         return sb.toString();
     }
+
 
 }
