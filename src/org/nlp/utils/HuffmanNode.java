160228e30b0d9a0759118d1d3cdcf21de2d3ae54
siegfang <siegfang@gmail.com>
2013-12-20 17:02:46 +0800

    first commit

---
+++ src/org/nlp/utils/HuffmanNode.java
@@ -0,0 +1,20 @@
+package org.nlp.utils;
+
+/**
+ * Created by fangy on 13-12-17.
+ * 哈夫曼树结点接口
+ */
+public interface HuffmanNode extends Comparable<HuffmanNode> {
+
+    public void setCode(int c);
+
+    public void setFrequency(int freq);
+
+    public int getFrequency();
+
+    public void setParent(HuffmanNode parent);
+
+    public HuffmanNode getParent();
+
+    public HuffmanNode merge(HuffmanNode sibling);
+}
