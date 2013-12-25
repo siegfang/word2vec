921d97da55fb91dc5191ce3a5bfe178df18c1df7
siegfang <siegfang@gmail.com>
2013-12-25 17:02:07 +0800

    Create WordNeuron.java

---
+++ src/org/nlp/vec/WordNeuron.java
@@ -0,0 +1,43 @@
+package org.nlp.vec;
+
+import com.nlp.util.HuffmanNode;
+import com.nlp.util.HuffmanTree;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by fangy on 13-12-17.
+ * 词神经元
+ */
+public class WordNeuron extends HuffmanNeuron {
+
+    private String name;
+    private List<HuffmanNode> pathNeurons;
+
+    public WordNeuron(String name, int freq, int vectorSize) {
+        super(freq, vectorSize);
+        this.name = name;
+        Random random = new Random();
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (random.nextDouble() - 0.5) / vectorSize;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<HuffmanNode> getPathNeurons() {
+        if (pathNeurons != null){
+            return pathNeurons;
+        }
+        pathNeurons = HuffmanTree.getPath(this);
+
+        return pathNeurons;
+    }
+}
