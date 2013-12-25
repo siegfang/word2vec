7e88df9690f9026b1d1e59598dbf52398a4184b1
siegfang <siegfang@gmail.com>
2013-12-25 09:27:01 +0800

    Create HuffmanNeuron.java

---
+++ src/org/vec/HuffmanNeuron.java
@@ -0,0 +1,72 @@
+package org.nlp.vec;
+
+import com.nlp.util.HuffmanNode;
+
+import java.util.Random;
+
+/**
+ * Created by fangy on 13-12-20.
+ */
+public class HuffmanNeuron implements HuffmanNode {
+
+    protected int frequency = 0;
+    protected HuffmanNode parentNeuron;
+    protected int code = 0;
+    protected double[] vector;
+
+    @Override
+    public void setCode(int c) {
+        code = c;
+    }
+
+    @Override
+    public void setFrequency(int freq) {
+        frequency = freq;
+    }
+
+    @Override
+    public int getFrequency() {
+        return frequency;
+    }
+
+    @Override
+    public void setParent(HuffmanNode parent) {
+        parentNeuron = parent;
+    }
+
+    @Override
+    public HuffmanNode getParent() {
+        return parentNeuron;
+    }
+
+    @Override
+    public HuffmanNode merge(HuffmanNode right){
+        HuffmanNode parent = new HuffmanNeuron(frequency+right.getFrequency(), vector.length);
+        parentNeuron = parent;
+        this.code = 0;
+        right.setParent(parent);
+        right.setCode(1);
+        return parent;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode hn) {
+
+        if (frequency > hn.getFrequency()){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public HuffmanNeuron(int freq, int vectorSize) {
+
+        this.frequency = freq;
+        this.vector = new double[vectorSize];
+        parentNeuron = null;
+        Random random = new Random();
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (random.nextDouble() - 0.5) / vectorSize;
+        }
+    }
+}
