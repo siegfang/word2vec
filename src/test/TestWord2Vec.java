76b08ca1aa13994bf27fe6f8265162c4d89b0374
siegfang <siegfang@gmail.com>
2014-01-09 10:14:56 +0800

    清晰一点

--- src/test/TestWord2Vec.java
+++ src/test/TestWord2Vec.java
@@ -15,43 +15,12 @@ import java.util.Set;
  */
 public class TestWord2Vec {
 
-    public static void readByUtil(){
-        Word2Vec wv = new Word2Vec.Factory().build();
-
-        String textFilePath = "Z:\\DataDeport\\novel_4j.txt";
-
-        LineIterator li = null;
-        try {
-
-            li = new LineIterator(
-                    new InputStreamReader(new FileInputStream(textFilePath)));
-            int lineCount = 0;
-            while (li.hasNext()){
-                wv.readTokens(new Tokenizer(li.nextLine(), " "));
-                lineCount ++;
-                if (lineCount > 2000){
-                    break;
-                }
-            }
-
-        } catch (FileNotFoundException fnfe){
-            fnfe.printStackTrace();
-        } finally{
-            LineIterator.closeQuietly(li);
-        }
-
-        wv.training();
-    }
-
-    public static void readByJava(){
+    public static void readByJava(String textFilePath, String modelFilePath){
 
         Word2Vec wv = new Word2Vec.Factory()
                 .setMethod(Word2Vec.Method.Skip_Gram)
-                .setNumOfThread(4).build();
+                .setNumOfThread(1).build();
 
-        String textFilePath = "D:\\library\\novel_4j.txt";
-
-//        BufferedReader br = null;
         try (BufferedReader br =
                      new BufferedReader(new FileReader(textFilePath))) {
             int lineCount = 0;
@@ -60,9 +29,7 @@ public class TestWord2Vec {
                 wv.readTokens(new Tokenizer(line, " "));
 //                System.out.println(line);
                 lineCount ++;
-//                if (lineCount > 200){
-//                    break;
-//                }
+
             }
 
         } catch (IOException ioe) {
@@ -70,42 +37,26 @@ public class TestWord2Vec {
         }
 
         wv.training();
-        wv.saveModel(new File("D:\\model\\novel_4j.nn"));
+        wv.saveModel(new File(modelFilePath));
     }
 
-    public static void testVector(){
+    public static void testVector(String modelFilePath){
 
-        VectorModel vm = VectorModel.loadFromFile(
-                "D:\\model\\novel_4j.nn");
+        VectorModel vm = VectorModel.loadFromFile(modelFilePath);
         Set<VectorModel.WordScore> result1 = Collections.emptySet();
-        Long startTime, endTime;
-        Set<VectorModel.WordScore> result2 = Collections.emptySet();
 
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++){
-//            result2 = vm.similar("脚");
-            result2 = vm.analogy("水", "火", "天");
+        result1 = vm.similar("亲");
+        for (VectorModel.WordScore we : result1){
+            System.out.println(we.name + " :\t" + we.score);
         }
-        endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime);
-
-//        result1 = vm.similar("脚");
-//        System.out.println(result1);
-//        System.out.println(result1.size());
-//
-//        result2 = vm.analogy("水", "火", "天");
-        System.out.println(result2);
-        //
-//        System.out.println(result.size());
-//        for (VectorModel.WordScore we : result2){
-//            System.out.println(we.name + " : " + we.score);
-//        }
     }
 
     public static void main(String[] args){
 
-//        readByJava();
-        testVector();
+        String textFilePath = "D:/data/corpus.dat";
+        String modelFilePath = "D:/data/corpus.nn";
+        readByJava(textFilePath, modelFilePath);
+        testVector(modelFilePath);
     }
 
 }
